1cff82199ec5cbf48517b991ebf0acd482dfac74
FelipeMelo <felipedemeloab@gmail.com>
2024-01-24 20:47:34 -0300

    atualizar

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 alert('Boas vindas ao jogo do número secreto');
-let numeroSecreto = 15;
+let numeroMaximo = 1000;
+let numeroSecreto = parseInt(Math.random() * numeroMaximo + 1);
 console.log(numeroSecreto);
 let chute;
 let tentativas = 1;
+
 //Enquanto chute não for igual ao numero secreto
 while (chute != numeroSecreto){
-    chute = prompt('Escolha um número entre 1 e 99');
+    chute = prompt(`Escolha um número entre 1 e ${numeroMaximo}`);
     // se chute for igual ao número secreto
     if (chute == numeroSecreto) {
-        alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativas`);
+        break;
     } else {
         if (chute > numeroSecreto) {
             alert(`O numero secreto é menor que ${chute}`);
@@ -19,3 +21,10 @@ while (chute != numeroSecreto){
         tentativas++
     }
 }
+let palavraTentatva = tentativas > 1 ? 'tentativas' : 'tentativa'
+alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} ${palavraTentatva}.`);
+//if (tentativas > 1){
+//    alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativas`);
+//} else {
+//    alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativa`);
+//}
